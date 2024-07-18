1a0b3d8ff0d337e117d48823559a1429abb61103
Ivan Ilichev <john.the.theologician@gmail.com>
2024-07-18 06:42:01 +0300

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Here we start!</h1>
+        <p>Here will be ultra mega web app</p>
+      </header>
+      <footer>
+        <p>
+          <span>Created with </span>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            React
+          </a>
+        </p>
+      </footer>
+    </div>
+  );
+}
+
+export default App;
